113a007267c4e1cc9a607ee8d32a7aece079002d
danilomota1988 <danilomota1988@gmail.com>
2025-02-04 18:17:27 -0300

    Final Aula 13

---
+++ features/steps/selecionar_produtos.py
@@ -0,0 +1,28 @@
+# 1 - Biblioteca / Imports
+import time
+from behave import given, when, then
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@given(u'que acesso o site Sauce Demo')
+def step_impl(context):
+    context.driver = webdriver.Chrome()                 # Instanciar o objeto Selenium WebDriber especializado para o Chrome
+    context.driver.maximize_window()                    # Maximizar a janela do navegador
+    context.driver.implicitly_wait(10)                  # Esperar
+    context.driver.get("https://www.saucedemo.com")     # abrir o navegador no endereço do site alvo
+
+@when(u'preencho os campos de login com usuario {usuario} e senha {senha}')
+def step_impl(context, usuario, senha):
+    context.driver.find_element(By.ID, "user-name").send_keys(usuario)
+    context.driver.find_element(By.ID, "password").send_keys(senha)
+    context.driver.find_element(By.ID, "login-button").click()
+
+@then(u'sou direcionado para a pagina Home')
+def step_impl(context):
+    assert context.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"
+    #time.sleep(5)
+    
+    # teardown / encerramento
+    context.driver.quit()
+
