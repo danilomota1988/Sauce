f32c33652b621415932345aa481fa5c8cfb3ca67
danilomota1988 <danilomota1988@gmail.com>
2025-02-03 21:39:20 -0300

    Updatde do código

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -21,5 +21,12 @@ class Teste_Produtos():
         self.driver.get(self.url)                                                   # abre o navegador
         self.driver.find_element(By.ID, "user-name").send_keys("standard_user")     # escreve no campo
         self.driver.find_element(By.ID, "password").send_keys("secret_sauce")       # escreve no campo
+        self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click()       # click no botão login
+
+        # Transição de tela
+
+        assert self.driver.find_element(By.CSS_SELECTOR, "span.title").text == "Products"          # COnfirma se o elemento tem o texto esperado
+        assert self.driver.find_element(By.CSS_SELECTOR, "div.page_wrapper div.inventory_container div.inventory_list div.inventory_item:nth-child(1) div.inventory_item_description div.inventory_item_label a:nth-child(1) > div.inventory_item_name").text == "Sauce Labs Backpack"          # COnfirma se o elemento tem o texto esperado
+        assert self.driver.find_element(By.CSS_SELECTOR, "div.page_wrapper div.inventory_container div.inventory_list div.inventory_item:nth-child(1) div.inventory_item_description div.pricebar > div.inventory_item_price").text == "$29.99"          # COnfirma se o elemento tem o texto esperado
 
 
